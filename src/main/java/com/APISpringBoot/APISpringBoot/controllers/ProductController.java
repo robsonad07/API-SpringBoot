3dfc8ee5e0ef5ee036608c88050b39b405b9df11
Robson <robsonad07@gmail.com>
2024-03-31 21:35:59 -0300

    metodo put para produtos implementado

--- src/main/java/com/APISpringBoot/APISpringBoot/controllers/ProductController.java
+++ src/main/java/com/APISpringBoot/APISpringBoot/controllers/ProductController.java
@@ -41,4 +41,18 @@ public class ProductController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(product.get());
     }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct(@PathVariable(value="id") UUID id,
+                                                @RequestBody @Valid ProductRecordDto productRecordDto){
+        Optional<ProductModel> product = productRepository.findById(id);
+        if(product.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+        }
+        var productModel = product.get();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
+
 }
